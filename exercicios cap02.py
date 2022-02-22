25d82c32c22062b7ec910c32bd1f60fd185eb511
Lucas Krause <lucas.rksr@gmail.com>
2022-02-22 14:59:18 -0300

    Add files via upload

---
+++ exercicios cap02.py
@@ -0,0 +1,49 @@
+##############################################################################
+# Parte do livro Introdução à Programação com Python
+# Autor: Nilo Ney Coutinho Menezes
+# Editora Novatec (c) 2010-2020
+# Primeira edição - Novembro/2010 - ISBN 978-85-7522-250-8
+# Segunda edição - Junho/2014 - ISBN 978-85-7522-408-3
+# Terceira Edição - Janeiro/2019 - ISBN 978-85-7522-718-3
+#
+# Site: https://python.nilo.pro.br/
+##############################################################################
+
+#cap. 02-01
+
+#a = 10 + (20*30)
+#print(a)
+
+#a = 42/30
+#print(a)
+
+#a = ((94+2)*6)-1
+#print(a)
+
+#cap. 02-02
+
+#a = ((10%3)*(10**2)+1-(10*4/2))
+#print(a)
+
+#cap. 02-03
+
+#print("Lucas Krause")
+
+#cap. 02-04
+
+#a = 3
+#b = 5
+#print(2*a*3*b)
+
+#cap. 02-05
+
+#a = 1
+#b = 2
+#c = 3
+#print(a+b+c)
+
+#cap.02-06
+
+#aumento = 115/100
+#salario = 750
+#print(salario*aumento)
