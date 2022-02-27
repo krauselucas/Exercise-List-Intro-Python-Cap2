4a0c82e8fbadcd8ae2e78c2b7fd59ad41f0bd602
Lucas Krause <lucas.rksr@gmail.com>
2022-02-27 16:57:46 -0300

    Update exercicios cap02.py

--- exercicios cap02.py
+++ exercicios cap02.py
@@ -12,6 +12,7 @@
 #cap. 02-01
 
 # Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python.
+
 # 10 + 20 × 30
 # 42 ÷ 30
 # (94 + 2) × 6 - 1
@@ -28,6 +29,7 @@
 #cap. 02-02
 
 # Digite a seguinte expressão no interpretador:
+
 # 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
 # Tente resolver o mesmo cálculo, usando apenas lápis e papel. Observe como a prioridade das operações é importante.
 
